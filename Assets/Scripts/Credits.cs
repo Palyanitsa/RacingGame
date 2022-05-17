efac70754d7cff87d51d91fbba95a527feee2d32
Palyanitsa <105170008+Palyanitsa@users.noreply.github.com>
2022-05-17 10:49:29 -0700

    anim

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour {
  
     public void Quit()
     {
         Debug.Log("Quit");
-        Application.Quit();
+        SceneManager.LoadScene("Menu");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //Application.Quit();
     }
 
 }
