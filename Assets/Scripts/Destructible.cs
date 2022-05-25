b5410de67c91737322f39f8131360fd9db79bdbb
Palyanitsa <105170008+Palyanitsa@users.noreply.github.com>
2022-05-25 08:40:43 -0700

    score

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Destructible : MonoBehaviour
 {
     public GameObject destroyedVersion;
+   
     
     void OnMouseDown()
     {
         Instantiate(destroyedVersion, transform.position, transform.rotation);
         Destroy(gameObject);
     } 
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/DestructiblePlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiblePlayer : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+   
+    
+    //void OnMouseDown()
+   // {
+   //     Instantiate(destroyedVersion, transform.position, transform.rotation);
+   //     Destroy(gameObject);
+   // } 
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
