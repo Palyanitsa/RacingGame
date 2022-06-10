5257aaad0e094b367d801b17d9deff6a4f6dd6b3
disk <34960270+diskordovich@users.noreply.github.com>
2022-06-10 19:05:01 +0300

    Ads

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -5,41 +5,23 @@ using UnityEngine.Advertisements;
 
 public class AdsManager : MonoBehaviour, IUnityAdsListener
 {
-    // Start is called before the first frame update
+#if UNITY_IOS
+    string gameId = "4792551";
+#else
+    string gameId = "4792569";
+#endif
+
+
     void Start()
     {
-        Advertisement.Initialize("4781869");
-        //Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId);
     }
 
     public void PlayAd()
     {
-        if (Advertisement.IsReady("video"))
+        if (Advertisement.IsReady("Interstitial_Android"))
         {
-            Advertisement.Show("video");
+            Advertisement.Show("Interstitial_Android");
         }
     }
-
-    /*public void OnUnityAdsReady(string placementId)
-    {
-        Debug.Log("Ads are ready");
-    }
-
-    public void OnUnityAdsDidError(string message)
-    {
-        Debug.Log("Error: " + message);
-    }
-
-    public void OnUnityAdsDidStart(string placementId)
-    {
-        Debug.Log("Video started");
-    }
-
-    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult) {
-        if (placementId == "rewardedVideo" && showResult == ShowResult.Finished)
-        {
-            Debug.Log("Player Should be rewarded!");
-        }
-    }
-    */
 }
